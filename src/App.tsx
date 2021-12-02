61e6e4a431c30c2cfc6535cdf32026fb52330742
ajdotguru <ajfsdeveloper@gmail.com>
2021-12-02 22:08:53 +0530

    react_query_demo: add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Container, Typography } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from 'utils/mui';
 
 export const App = () => (
 	<ThemeProvider theme={theme}>
-		<h1>App</h1>
+		<Container maxWidth="xl">
+			<Typography variant="h1">App</Typography>
+		</Container>
 	</ThemeProvider>
 );
